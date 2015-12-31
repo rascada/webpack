8c6f91e983db98269531c674f8c4717cbbcc44f2
Damian <carftjr@gmail.com>
2015-12-31 16:26:39 +0100

    remove hello

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,22 +1,3 @@
-<template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-  </div>
+<template lang='jade'>
+  app
 </template>
-
-<script>
-import Hello from './components/Hello'
-
-export default {
-  components: {
-    Hello
-  }
-}
-</script>
-
-<style>
-body {
-  font-family: Helvetica, sans-serif;
-}
-</style>
