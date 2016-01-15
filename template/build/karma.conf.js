ad99492d183c12326b932d7a3f5e08baa840b3bc
rascada <carftjr@gmail.com>
2016-01-15 20:23:25 +0100

    tape tests

--- template/build/karma.conf.js
+++ template/build/karma.conf.js
@@ -3,14 +3,28 @@ delete webpackConf.entry;
 
 module.exports = function(config) {
   config.set({
+    plugins: [
+      require('karma-tap'),
+      require('karma-webpack'),
+      require('karma-spec-reporter'),
+      require('karma-phantomjs-launcher'),
+    ],
+
+    files: ['../test/*.js'],
     browsers: ['PhantomJS'],
     frameworks: ['tap'],
     reporters: ['spec'],
-    files: ['../test/unit/index.js'],
+
     preprocessors: {
-      '../test/unit/index.js': ['webpack'],
+      '../test/*.js': ['webpack'],
     },
-    webpack: webpackConf,
+
+    webpack: Object.assign({
+      node: {
+        fs: 'empty',
+      },
+    }, webpackConf),
+
     webpackMiddleware: {
       noInfo: true,
     },
